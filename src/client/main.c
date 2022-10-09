9b27b22bdc85f8d7a4e739e96ca45ec182ddae4a
daniel.source <79308609+danielsource@users.noreply.github.com>
2022-10-09 10:22:21 -0300

    Update front-end

--- src/client/main.c
+++ src/client/main.c
@@ -1,23 +1,29 @@
 #include "raylib.h"
 
-static int screen_width = 500;
-static int screen_height = 360;
-
-static Rectangle canvas = {
-	.x = 30,
-	.y = 30,
-	.width = 300,
-	.height = 300
-};
-
 int
 main(void) {
+	int margin = 15;
+	int screen_width = 400;
+	int screen_height = 250;
+	Rectangle canvas = {
+		.x = margin,
+		.y = margin,
+		.width = screen_height - margin*2,
+		.height = screen_height - margin*2
+	};
+
 	SetTraceLogLevel(LOG_WARNING);
-	InitWindow(screen_width, screen_height, "My first multiplayer game");
+	InitWindow(screen_width, screen_height,
+			"My first multiplayer game");
 	SetTargetFPS(60);
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(LIGHTGRAY);
+		DrawText("Top 10 players", canvas.x + canvas.width +
+				margin, margin, 10, BLACK);
+		DrawText("Points", screen_width - margin -
+				MeasureText("Points", 10), margin, 10,
+				BLACK);
 		DrawRectangleRec(canvas, WHITE);
 		EndDrawing();
 	}
