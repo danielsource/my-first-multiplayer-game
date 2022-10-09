f353e2259fb9bc86ee9253caa2f975896e9f7a98
daniel.source <79308609+danielsource@users.noreply.github.com>
2022-10-09 11:05:16 -0300

    Add cool shadow

--- src/client/main.c
+++ src/client/main.c
@@ -1,4 +1,16 @@
+#include <stdint.h>
+
 #include "raylib.h"
+
+static Color
+rgba_int_to_color(uint32_t i) {
+	Color c;
+	c.r = i >> 24;
+	c.g = i >> 16 & 0xff;
+	c.b = i >> 8  & 0xff;
+	c.a = i       & 0xff;
+	return c;
+}
 
 int
 main(void) {
@@ -11,6 +23,11 @@ main(void) {
 		.width = screen_height - margin*2,
 		.height = screen_height - margin*2
 	};
+	Color col_bg         = rgba_int_to_color(0xeeeeeeff);
+	Color col_canvas     = rgba_int_to_color(0xffffffff);
+	Color col_fg         = rgba_int_to_color(0x000000ff);
+	Color col_player     = rgba_int_to_color(0xbdaa27ff);
+	Color col_scoreboard = rgba_int_to_color(0xeeeeeeff);
 
 	SetTraceLogLevel(LOG_WARNING);
 	InitWindow(screen_width, screen_height,
@@ -18,13 +35,28 @@ main(void) {
 	SetTargetFPS(60);
 	while (!WindowShouldClose()) {
 		BeginDrawing();
-		ClearBackground(LIGHTGRAY);
+		ClearBackground(col_bg);
+
+		// Draw shadow
+		Rectangle shadow = canvas;
+		float alpha = 0.03f;
+		for (int i = 0; alpha > 0; i++) {
+			shadow.x -= 1;
+			shadow.y -= 1;
+			shadow.width += 2;
+			shadow.height += 2;
+			DrawRectangleRounded(shadow, 0.09f, 0, Fade(BLACK, alpha));
+			alpha -= 0.001f * i;
+		}
+
+		// Draw scoreboard
 		DrawText("Top 10 players", canvas.x + canvas.width +
-				margin, margin, 10, BLACK);
+				margin, margin, 10, col_fg);
 		DrawText("Points", screen_width - margin -
 				MeasureText("Points", 10), margin, 10,
-				BLACK);
-		DrawRectangleRec(canvas, WHITE);
+				col_fg);
+
+		DrawRectangleRec(canvas, col_canvas);
 		EndDrawing();
 	}
 	CloseWindow();
